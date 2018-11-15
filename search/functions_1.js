18bf032c304c4d5cd9e60029be47071598ca3883
Michele Caini <michele.caini@gmail.com>
2018-11-15 23:21:44 +0100

    API reference v1.1.0

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['base',['base',['../classmeta_1_1factory.html#a098a7aee7a2cbb370615056484991e39',1,'meta::factory::base()'],['../classmeta_1_1base.html#a2b79022835b5a14644d8dbda265e7055',1,'meta::base::base()'],['../classmeta_1_1type.html#a61767606a75555ed1a2bf11b7e66416b',1,'meta::type::base(Op op) const noexcept'],['../classmeta_1_1type.html#a88d11c14bcd173bdba1cfbe703805fbd',1,'meta::type::base(const char *str) const noexcept']]]
+  ['base',['base',['../classmeta_1_1factory.html#ac5f1ec5b9a1cefcb2b6e5aaaa72b6e82',1,'meta::factory::base()'],['../classmeta_1_1base.html#a2b79022835b5a14644d8dbda265e7055',1,'meta::base::base()'],['../classmeta_1_1type.html#a61767606a75555ed1a2bf11b7e66416b',1,'meta::type::base(Op op) const noexcept'],['../classmeta_1_1type.html#a88d11c14bcd173bdba1cfbe703805fbd',1,'meta::type::base(const char *str) const noexcept']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['func',['func',['../classmeta_1_1factory.html#ae394af8a04c5b7e5403465d7f13905f6',1,'meta::factory::func()'],['../classmeta_1_1func.html#ae35b61cc1d57307351796d2c329a107f',1,'meta::func::func()'],['../classmeta_1_1type.html#a75073faefb2dba9ccb2e680d1ebc64ec',1,'meta::type::func(Op op) const noexcept'],['../classmeta_1_1type.html#a4c3ad87be4eed4a5e33b741f9c5c7d3c',1,'meta::type::func(const char *str) const noexcept']]]
+  ['func',['func',['../classmeta_1_1factory.html#abf9bee39928f05a99a92e7e2535184e3',1,'meta::factory::func()'],['../classmeta_1_1func.html#ae35b61cc1d57307351796d2c329a107f',1,'meta::func::func()'],['../classmeta_1_1type.html#a75073faefb2dba9ccb2e680d1ebc64ec',1,'meta::type::func(Op op) const noexcept'],['../classmeta_1_1type.html#a4c3ad87be4eed4a5e33b741f9c5c7d3c',1,'meta::type::func(const char *str) const noexcept']]]
 ];
