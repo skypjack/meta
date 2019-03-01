fac2c7479a78f013e1665eca6f3fba0649976694
Michele Caini <michele.caini@gmail.com>
2019-03-01 17:46:27 +0100

    API reference v1.2.0

--- search/all_6.js
+++ search/all_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['handle',['handle',['../classmeta_1_1handle.html',1,'meta::handle'],['../classmeta_1_1any.html#ad5131fca8411651b7eaed9ca2b4a6fef',1,'meta::any::handle()'],['../classmeta_1_1handle.html#ab7c0f9a690b469445ebe018dc3e5b06c',1,'meta::handle::handle() noexcept'],['../classmeta_1_1handle.html#a774897a65afe5f9c8b8a8633e9aa9e59',1,'meta::handle::handle(Type &amp;&amp;instance) noexcept'],['../classmeta_1_1handle.html#a031ecb08c6187fc789163011198fa8df',1,'meta::handle::handle(const handle &amp;) noexcept=default'],['../classmeta_1_1handle.html#a51599d1f0dd2afaab64db1833213ae5e',1,'meta::handle::handle(handle &amp;&amp;) noexcept=default']]],
+  ['handle',['handle',['../classmeta_1_1handle.html',1,'meta::handle'],['../classmeta_1_1any.html#ad5131fca8411651b7eaed9ca2b4a6fef',1,'meta::any::handle()'],['../classmeta_1_1handle.html#ab7c0f9a690b469445ebe018dc3e5b06c',1,'meta::handle::handle() noexcept'],['../classmeta_1_1handle.html#a774897a65afe5f9c8b8a8633e9aa9e59',1,'meta::handle::handle(Type &amp;&amp;instance) noexcept']]],
   ['header_2donly_20runtime_20reflection_20system_20in_20c_2b_2b',['Header-only runtime reflection system in C++',['../index.html',1,'']]]
 ];
